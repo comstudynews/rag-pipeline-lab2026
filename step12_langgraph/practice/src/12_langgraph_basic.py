e56c0442be448cd278695a2c7f7ba95ceccb6ea5
BEOMJOON KIM <96456853+comstudynews@users.noreply.github.com>
2026-09-24 15:07:52 +0900

    Sync practice examples with RAG cookbook

--- step12_langgraph/practice/src/12_langgraph_basic.py
+++ step12_langgraph/practice/src/12_langgraph_basic.py
@@ -10,14 +10,32 @@ class GraphState(TypedDict):
 
 
 def normalize_question(state: GraphState):
-    # TODO: question의 앞뒤 공백을 제거해 normalized_question을 반환하세요.
+    # TODO 1: question의 앞뒤 공백을 제거해 normalized_question을 반환하세요.
     raise NotImplementedError
 
 
 def count_length(state: GraphState):
-    # TODO: normalized_question의 길이를 계산해 length를 반환하세요.
+    # TODO 2: normalized_question의 길이를 계산해 length를 반환하세요.
     raise NotImplementedError
 
 
-# TODO: StateGraph에 Node, Edge, Conditional Edge를 연결하세요.
+def short_question(state: GraphState):
+    print("짧은 질문입니다.")
+    return {}
+
+
+def long_question(state: GraphState):
+    print("긴 질문입니다.")
+    return {}
+
+
+def route_by_length(state: GraphState):
+    # TODO 3: length <= 20이면 short, 아니면 long을 반환하세요.
+    raise NotImplementedError
+
+
+# TODO 4: StateGraph에 Node와 Edge를 등록하세요.
+# TODO 5: count_length 뒤에 Conditional Edge를 연결하세요.
+# TODO 6: short / long을 END에 연결하고 compile/invoke하세요.
+
 print("TODO: Step 12 LangGraph를 완성하세요.")
