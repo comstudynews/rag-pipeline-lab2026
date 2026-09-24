44e60d6d7e3763d60115cac85ba15f9f31a79d94
BEOMJOON KIM <96456853+comstudynews@users.noreply.github.com>
2026-09-24 15:07:26 +0900

    Sync practice examples with RAG cookbook

--- step02_document_loader/practice/src/02_loader.py
+++ step02_document_loader/practice/src/02_loader.py
@@ -1,9 +1,17 @@
 from langchain_community.document_loaders import TextLoader
 
-# TODO: data/sample.txt를 TextLoader로 읽어 docs에 저장하세요.
+# TODO: data/sample.txt를 UTF-8로 읽는 TextLoader를 만드세요.
+loader = None
+
+# TODO: loader.load() 결과를 docs에 저장하세요.
 docs = []
 
-print("Document 수:", len(docs))
-for i, doc in enumerate(docs, start=1):
-    print(f"\n--- Document {i} ---")
-    print(doc.page_content)
+print("문서 개수:", len(docs))
+
+if docs:
+    print("\n[page_content]")
+    print(docs[0].page_content)
+    print("\n[metadata]")
+    print(docs[0].metadata)
+else:
+    print("TODO: TextLoader를 완성하세요.")
